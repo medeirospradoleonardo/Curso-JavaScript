4b706baf06e03d31d4e6c96277e21b789e72ab2c
Leonardo Medeiros Prado <medeiros.prado@unesp.br>
2021-10-05 12:54:33 -0300

    Curso JavaScript

--- exercicios-js/array/arrayMetodos.js
+++ exercicios-js/array/arrayMetodos.js
@@ -10,3 +10,19 @@ console.log(pilotos)
 
 pilotos.unshift('Hamilton') // insere um elemento na primeira posição da lista
 console.log(pilotos)
+
+// splice pode adicionar ou remover elementos
+
+// adicionar
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+// remover
+pilotos.splice(3, 1) // Massa quebrou novamente
+console.log(pilotos)
+
+const algunsPilotos1 = pilotos.slice(2) // Novo Array
+console.log(algunsPilotos1)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
