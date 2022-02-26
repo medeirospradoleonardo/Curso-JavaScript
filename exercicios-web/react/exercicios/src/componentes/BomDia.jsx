6cc8fc4bdf931868fc7d8bacd28999947f5ef028
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-02-26 04:50:32 -0300

    Componente Com Parâmetro

--- exercicios-web/react/exercicios/src/componentes/BomDia.jsx
+++ exercicios-web/react/exercicios/src/componentes/BomDia.jsx
@@ -1,18 +1,21 @@
 import React from "react"
 
-export default props => [
+const func = props => [
         <h1 key='h1'>Bom dia, {props.nome}!</h1>,
         <h2 key='h2'>Até breve!</h2>
 ]
 
-// export default props => 
+
+// const func = props => 
 //     <React.Fragment>
 //         <h1>Bom dia, {props.nome}!</h1>
 //         <h2>Até breve!</h2>
 //     </React.Fragment>
 
-// export default props => 
+// const func = props => 
 //     <div>
 //         <h1>Bom dia, {props.nome}!</h1>
 //         <h2>Até breve!</h2>
 //     </div>
+
+export default func
