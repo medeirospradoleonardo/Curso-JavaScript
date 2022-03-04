ba538485f6b8a09267e3b5ba6e6edd0b0591ad80
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-03-04 04:39:48 -0300

    Componente Navegador

--- exercicios-web/nextjs/fundamentos/src/pages/exemplo.jsx
+++ exercicios-web/nextjs/fundamentos/src/pages/exemplo.jsx
@@ -1,10 +1,12 @@
-import Cabecalho from "../components/cabecalho"
+import Cabecalho from '../components/cabecalho'
+import Layout from '../components/Layout'
+
 export default function Exemplo(){
     // props é somente leitura
     return(
-        <>
+        <Layout titulo="Usando Componentes">
             <Cabecalho titulo="Next.js & React" />
             <Cabecalho titulo="Aprenda Next na prática" />
-        </>
+        </Layout>
     )
 }
--- exercicios-web/nextjs/fundamentos/src/pages/jsx.jsx
+++ exercicios-web/nextjs/fundamentos/src/pages/jsx.jsx
@@ -1,17 +1,21 @@
-export default function Jsx(){
+import Layout from '../components/Layout'
+
+export default function Jsx() {
     const titulo = <h1>JSX é um conceito central</h1>
 
     function subtitulo() {
         return <h2>{"Muito legal".toUpperCase()}</h2>
     }
 
-    return(
-        <div>
-            {titulo}
-            {subtitulo()}
-            <p>
-                {JSON.stringify({nome: 'João', idade: 30})}
-            </p>
-        </div>
+    return (
+        <Layout titulo="Entendendo o JSX">
+            <div>
+                {titulo}
+                {subtitulo()}
+                <p>
+                    {JSON.stringify({ nome: 'João', idade: 30 })}
+                </p>
+            </div>
+        </Layout>
     )
 }
