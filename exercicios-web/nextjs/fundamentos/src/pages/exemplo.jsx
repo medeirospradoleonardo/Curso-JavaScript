285af306c25d9a954bd7d3b871c0514c374a5546
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-03-04 03:34:57 -0300

    Usando Componente

---
+++ exercicios-web/nextjs/fundamentos/src/pages/exemplo.jsx
@@ -0,0 +1,9 @@
+import Cabecalho from "../components/cabecalho"
+export default function Exemplo(){
+    return(
+        <>
+            <Cabecalho />
+            <Cabecalho />
+        </>
+    )
+}
