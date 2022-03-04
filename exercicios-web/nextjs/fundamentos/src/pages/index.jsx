01000b3732865284e584912a6674bb53ba704676
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-03-04 02:30:47 -0300

    Entendendo o JSX

--- exercicios-web/nextjs/fundamentos/src/pages/index.jsx
+++ exercicios-web/nextjs/fundamentos/src/pages/index.jsx
@@ -1,3 +1,9 @@
 export default function Inicio(){
-    return "Início 2"
+    // "<h1>Título</h1>>"
+    return (
+        <div>
+            <h1>Fundamentos de Next.js & React</h1>
+            <h2>Vamos estudar esse framework</h2>
+        </div>
+    )
 }
