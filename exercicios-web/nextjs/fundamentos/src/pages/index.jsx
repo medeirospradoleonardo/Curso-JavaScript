46cd6b630a0f9f41d4237d20376aa99ad281df0a
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-03-06 05:17:24 -0300

    Recomendação de Vídeo

--- exercicios-web/nextjs/fundamentos/src/pages/index.jsx
+++ exercicios-web/nextjs/fundamentos/src/pages/index.jsx
@@ -14,8 +14,8 @@ export default function Inicio(){
             <Navegador texto="JSX" destino="/jsx" cor="crimson" />
             <Navegador texto="Navegação #01" destino="/navegacao" cor="green" />
             <Navegador texto="Navegação #02" destino="/cliente/sp-2/123" cor="blue" />
-            <Navegador texto="Componente com Estado" destino="/estado" cor="pink" />
-            <Navegador texto="Integração com API #01" destino="/integracao_1" cor="yellow" />
+            <Navegador texto="Componente com Estado" destino="/estado" cor="#a45b71" />
+            <Navegador texto="Integração com API #01" destino="/integracao_1" cor="#42a9a9" />
         </div>
     )
 }
