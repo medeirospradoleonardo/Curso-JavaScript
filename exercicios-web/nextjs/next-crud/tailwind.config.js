b9632a8b856842a8202d26cbeaffab6a20baef4d
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-03-07 23:11:22 -0300

    Componente Botão

--- exercicios-web/nextjs/next-crud/tailwind.config.js
+++ exercicios-web/nextjs/next-crud/tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    {
+      pattern: /-/,
+      variants: ['bg', 'to', 'from'],
+    },
+  ],
   theme: {
     extend: {},
   },
